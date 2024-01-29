fd0917442de7216f3ba32430853081c3c9c4110f
Todd DeMelle <tdemelle@stuffisparts.com>
2024-01-29 13:05:02 -0500

    attempting to resolve postinstall issues with github copilot.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2021,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "rules": {
+    // Add your custom rules here
+  }
+};
